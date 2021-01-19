51cc885ea6491856837466c3e0230dcaced3e55d
Javier Cortejoso <javier.cortejoso@gmail.com>
2021-01-19 14:44:44 +0100

    Using default ganache privateKey

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
         faucetTimeSpan: process.env.FAUCET_TIMESPAN || 24,
         faucetNode: process.env.FAUCET_NODE || 'http://localhost:8545',
         faucetGas: process.env.FAUCET_GAS || '21000',
-        privateKey: process.env.FAUCET_PRIVATE_KEY
+        privateKey: process.env.FAUCET_PRIVATE_KEY || '878b0bb351b9aa89a18aae4b910839611980eae2aaec160abaa042f975ea81c5'
     },
     database: {
         uri: process.env.ELASTIC_URL || 'http://localhost:9200',
